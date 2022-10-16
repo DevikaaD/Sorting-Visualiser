c6f480c4fbfd49d4097d1b16cf580b4483905378
DevikaaD <devikaapreethi2000@gmail.com>
2022-10-16 10:27:49 +0530

    Visualiser

---
+++ build/precache-manifest.fadeddd9ad0eeae8360927de849e728f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c30b320ecac330f1aa04",
+    "url": "/static/css/main.4213d5fd.chunk.css"
+  },
+  {
+    "revision": "c30b320ecac330f1aa04",
+    "url": "/static/js/main.c30b320e.chunk.js"
+  },
+  {
+    "revision": "029a94e7821f85f5d3db",
+    "url": "/static/js/1.029a94e7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "059ff57ebb23c32823c10e416509c9d5",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component }  from 'react';
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+          <Header />
+          <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
